2f3f77bf0e283a7b9d5b7153337831d8eb878063
DasAleixo <contato.victorzmr@gmail.com>
2024-01-31 18:11:25 -0300

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -37,12 +37,12 @@ function App() {
   const { indexStep, currentComponent, changeStep, isLastStep, isFirstStep } = useForm(formComponents);
 
   return (
-    <div className='p-8' id='app'>
+    <div className='p-8 max-w-md sm:max-w-2xl' id='app'>
       <div id='header' className="text-center mb-8">
         <h2 className="text-3xl font-bold mb-6">Deixe sua avaliação</h2>
         <p className="text-zinc-500 text-sm2">Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto</p>
       </div>
-      <div id='form-container' className="max-w-xl bg-neutral-50 p-16 shadow-xl mx-0 my-auto">
+      <div id='form-container' className="max-w-xl bg-neutral-50 sm:p-16 p-8 shadow-xl mx-0 my-auto">
         <Steps currentStep={indexStep} />
         <form onSubmit={(e) => changeStep(indexStep + 1, e)} className="">
           <div id="inputs-container">
--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -7,21 +7,21 @@ const ReviewForm = ({ data, updateFieldHandler }) => {
 
   return (
     <div id="review-form">
-      <div id="form-control" className="flex gap-10 mt-10">
+      <div id="form-control" className="flex sm:gap-10 gap-5 mt-10 justify-center items-center">
 
         <label className="flex flex-col justify-center items-center">
           <input id="ms" type="radio" name="review" className="peer opacity-0" value="very_satisfied"
             onChange={(e) => updateFieldHandler("review", e.target.value)}
             checked={data.review === "very_satisfied"} required />
-          <BsEmojiGrin id="very_satisfied" className={`size-7 mb-2 cursor-pointer hover:fill-[#9370DB] transition fill-current peer-checked:fill-[#9370DB]`} />
-          <p>Muito satisfeito</p>
+          <BsEmojiGrin id="very_satisfied" className={`sm:size-7 size-6 mb-2 cursor-pointer hover:fill-[#9370DB] transition fill-current peer-checked:fill-[#9370DB]`} />
+          <p>Adorei</p>
         </label>
 
         <label name="review" className="flex flex-col justify-center items-center">
           <input id="s" type="radio" name="review" className="peer opacity-0" value="satisfied"
             onChange={(e) => updateFieldHandler("review", e.target.value)}
             checked={data.review === "satisfied"} required />
-          <BsEmojiSmile className="size-7 mb-2 cursor-pointer hover:fill-[#9370DB] transition fill-current peer-checked:fill-[#9370DB]" />
+          <BsEmojiSmile className="sm:size-7 size-6 mb-2 cursor-pointer hover:fill-[#9370DB] transition fill-current peer-checked:fill-[#9370DB]" />
           <p>Satisfeito</p>
         </label>
 
@@ -29,7 +29,7 @@ const ReviewForm = ({ data, updateFieldHandler }) => {
           <input id="n" type="radio" name="review" className="peer opacity-0" value="neutral"
             onChange={(e) => updateFieldHandler("review", e.target.value)}
             checked={data.review === "neutral"} required />
-          <BsEmojiNeutral className="size-7 mb-2 cursor-pointer hover:fill-[#9370DB] transition fill-current peer-checked:fill-[#9370DB]" />
+          <BsEmojiNeutral className="sm:size-7 size-6 mb-2 cursor-pointer hover:fill-[#9370DB] transition fill-current peer-checked:fill-[#9370DB]" />
           <p>Neutro</p>
         </label>
 
@@ -37,7 +37,7 @@ const ReviewForm = ({ data, updateFieldHandler }) => {
           <input id="i" type="radio" name="review" className="peer opacity-0" value="insatisfied"
             onChange={(e) => updateFieldHandler("review", e.target.value)}
             checked={data.review === "insatisfied"} required />
-          <BsEmojiFrown className="size-7 mb-2 cursor-pointer hover:fill-[#9370DB] transition fill-current peer-checked:fill-[#9370DB]" />
+          <BsEmojiFrown className="sm:size-7 size-6 mb-2 cursor-pointer hover:fill-[#9370DB] transition fill-current peer-checked:fill-[#9370DB]" />
           <p>Insatisfeito</p>
         </label>
       </div>
